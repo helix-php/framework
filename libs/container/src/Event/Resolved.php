ea72be352b3c5f3b7db22fb3f705395971bc740e
SerafimArts <nesk@xakep.ru>
2022-05-25 11:27:58 +0300

    Add twig bridge and improve service loading

---
+++ libs/container/src/Event/Resolved.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of Helix package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Helix\Container\Event;
+
+use Helix\Contracts\Container\DefinitionInterface;
+
+/**
+ * @template T of object
+ * @template-extends ServiceEvent<T>
+ */
+class Resolved extends ServiceEvent
+{
+    /**
+     * @param DefinitionInterface<T> $definition
+     * @param T $service
+     */
+    public function __construct(
+        DefinitionInterface $definition,
+        public readonly object $service,
+    ) {
+        parent::__construct($definition);
+    }
+}
